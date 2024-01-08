00093697c0dfebbf81ca5542166569b2bdcda082
bijink <kbijin528.pro@gmail.com>
2024-01-08 16:37:02 +0530

    Home screen

--- app/index.js
+++ app/index.js
@@ -1,10 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, SIZES, icons, images } from "../constants";
+import { Nearbyjobs, Popularjobs, Welcome, ScreenHeaderBtn } from "../components";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />,
+          headerRight: () => <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />,
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
